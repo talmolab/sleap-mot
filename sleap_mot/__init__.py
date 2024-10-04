77d0d5fbddbb55b2bae7914aeb0c11e57da2ecf5
Talmo Pereira <talmo@salk.edu>
2024-10-04 16:46:01 -0700

    Fix imports

--- sleap_mot/__init__.py
+++ sleap_mot/__init__.py
@@ -1,3 +1,12 @@
 """This module exposes all high level APIs for sleap-io."""
 
 from sleap_mot.version import __version__
+from sleap_mot import tracker, track_instance, candidates, utils
+from sleap_mot.tracker import Tracker, FlowShiftTracker
+from sleap_mot.track_instance import (
+    TrackedInstanceFeature,
+    TrackInstanceLocalQueue,
+    TrackInstances,
+)
+from sleap_mot.candidates.local_queues import LocalQueueCandidates
+from sleap_mot.candidates.fixed_window import FixedWindowCandidates
